1e24f81cc57ca98bf979e7cea50eaebf64fff50b
Lester Vecsey <service@paychess.com>
2012-04-05 13:15:40 -0400

    import cmd_magic.c and cmd_magic.h

---
+++ src/cmd_magic.c
@@ -0,0 +1,6 @@
+
+#include <sys/types.h>
+
+u_int64_t cmd_turnon = 0x20485041;
+u_int64_t cmd_turnoff = 0x20485240;
+
---
+++ src/cmd_magic.h
@@ -0,0 +1,8 @@
+#ifndef CMDMAGIC_H
+#define CMDMAGIC_H
+
+#include <sys/types.h>
+
+extern u_int64_t cmd_turnon, cmd_turnoff;
+
+#endif
